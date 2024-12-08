e357a5442facebb69d494b59fdbeb5e0ad4ff1d6
markuysa_w <bwg.it603@gmail.com>
2024-12-08 22:45:41 +0300

    validate

--- redis/redis.go
+++ redis/redis.go
@@ -6,6 +6,6 @@ type Config struct {
 	Host     string        `validate:"required" envconfig:"REDIS_HOST" default:"localhost"`
 	Port     int           `validate:"required" envconfig:"REDIS_PORT" default:"6379"`
 	Password string        `validate:"required" envconfig:"REDIS_PASSWORD"`
-	DB       int           `validate:"required" envconfig:"REDIS_DB" default:"0"`
+	DB       int           `envconfig:"REDIS_DB" default:"0"`
 	Timeout  time.Duration `validate:"required" envconfig:"REDIS_TIMEOUT" default:"5s"`
 }
