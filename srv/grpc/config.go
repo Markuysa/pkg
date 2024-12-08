ab1056c3aabe81cfcde1eedf154c6e17a2afd251
markuysa_w <bwg.it603@gmail.com>
2024-12-08 23:09:49 +0300

    added tags

--- srv/grpc/config.go
+++ srv/grpc/config.go
@@ -3,11 +3,11 @@ package grpc
 import "time"
 
 type Config struct {
-	Host              string        `default:"0.0.0.0:9000"`
-	MaxConnectionIdle time.Duration `default:"5m"`
-	MaxConnectionAge  time.Duration `default:"5m"`
-	Timeout           time.Duration `default:"5m"`
-	Time              time.Duration `default:"5m"`
-	MaxRecvMsgSize    int
-	MaxSendMsgSize    int
+	Host              string        `default:"0.0.0.0:9000" yaml:"host"`
+	MaxConnectionIdle time.Duration `default:"5m" yaml:"max_connection_idle"`
+	MaxConnectionAge  time.Duration `default:"5m" yaml:"max_connection_age"`
+	Timeout           time.Duration `default:"5m" yaml:"timeout"`
+	Time              time.Duration `default:"5m" yaml:"time"`
+	MaxRecvMsgSize    int           `default:"10485760" yaml:"max_recv_msg_size"`
+	MaxSendMsgSize    int           `default:"10485760" yaml:"max_send_msg_size"`
 }
