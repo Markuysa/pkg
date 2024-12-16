0dce26480e8e07de18ff1e5c001a444bc928da1c
markuysa_w <bwg.it603@gmail.com>
2024-12-16 15:02:30 +0300

    added tags:

--- consul/config.go
+++ consul/config.go
@@ -2,21 +2,21 @@ package consul
 
 type (
 	Config struct {
-		Address      string
-		ServiceExtra ServiceExtra
+		Address      string       `yaml:"address"`
+		ServiceExtra ServiceExtra `yaml:"service_extra"`
 	}
 	ServiceExtra struct {
-		ServiceID  string `default:"unknown"`
-		Name       string `default:"unknown"`
-		ExposePort int    `default:"8000"`
-		Address    string
-		Tags       []string
-		Namespace  string `default:"default"`
-		Probe      Probe
+		ServiceID  string   `default:"unknown" yaml:"service_id"`
+		Name       string   `default:"unknown" yaml:"name"`
+		ExposePort int      `default:"8000" yaml:"expose_port"`
+		Address    string   `yaml:"address"`
+		Tags       []string `yaml:"tags"`
+		Namespace  string   `yaml:"namespace" default:"default"`
+		Probe      Probe    `yaml:"probe"`
 	}
 	Probe struct {
-		HealthCheck string
-		Timeout     string `default:"30s"`
-		Interval    string `default:"10s"`
+		HealthCheck string `yaml:"health_check"`
+		Timeout     string `default:"30s" yaml:"timeout"`
+		Interval    string `default:"10s" yaml:"interval"`
 	}
 )
